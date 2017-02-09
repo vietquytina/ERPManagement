08ddbcb366b12ea48d3a69dedeb0372e246f0a3b
vietquytina <vietquytina@lab.birdie.co.jp>
2017-02-09 14:26:44 +0700

    Add EquipmentType view model.

---
+++ ERPManagement/ERPManagement/ViewModel/List/EquipmentTypeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ERPManagement.ViewModel.List
+{
+    class EquipmentTypeViewModel : ListViewModel
+    {
+
+    }
+}
