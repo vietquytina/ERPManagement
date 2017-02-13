37020e9ef5e561e2aba44a97b634fe87d4e93beb
vietquytina <vietquytina@lab.birdie.co.jp>
2017-02-13 11:14:08 +0700

    add EquipmentViewModel

---
+++ ERPManagement/ERPManagement/ViewModel/Equipment/EquipmentViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Telerik.Windows.Controls;
+
+namespace ERPManagement.ViewModel.Equipment
+{
+    class EquipmentViewModel : BaseViewModel
+    {
+        #region Variables
+        private String number, note;
+        private DateTime date = DateTime.Now;
+        #endregion
+
+        #region Properties
+        public String Number
+        {
+            get { return number; }
+            set
+            {
+                if (number != value)
+                {
+                    number = value;
+                    RaisePropertyChanged("Number");
+                }
+            }
+        }
+
+        public String Note
+        {
+            get { return note; }
+            set
+            {
+                if (note != value)
+                {
+                    note = value;
+                    RaisePropertyChanged("Note");
+                }
+            }
+        }
+
+        public DateTime Date
+        {
+            get { return date; }
+            set
+            {
+                if (date != value)
+                {
+                    date = value;
+                    RaisePropertyChanged("Date");
+                }
+            }
+        }
+        #endregion
+    }
+}
