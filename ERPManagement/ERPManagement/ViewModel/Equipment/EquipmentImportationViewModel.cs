7d1c204a65c80f483d2c747aed5565d2503b48ec
vietquytina <vietquytina@lab.birdie.co.jp>
2017-02-13 17:14:43 +0700

    + add EquipmentImportationViewModel

---
+++ ERPManagement/ERPManagement/ViewModel/Equipment/EquipmentImportationViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Telerik.Windows.Controls;
+
+namespace ERPManagement.ViewModel.Equipment
+{
+    class EquipmentImportationViewModel : EquipmentViewModel
+    {
+
+        protected override void Save(RadWindow window)
+        {
+        }
+
+        protected override bool Delete()
+        {
+            return base.Delete();
+        }
+
+        protected override void Edit()
+        {
+
+        }
+    }
+}
