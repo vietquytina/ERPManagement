bfccdf7fd039aba20564693a0be4b7fb6aaaa06a
vietquytina <vietquytina@lab.birdie.co.jp>
2017-02-17 17:22:17 +0700

    + add EquipmentBreakViewModel, Exportation.

---
+++ ERPManagement/ERPManagement/ViewModel/Equipment/EquipmentBreakViewModel.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ERPManagement.ViewModel.Equipment
+{
+    class EquipmentBreakDetailViewModel : EquipmentDetailViewModel
+    {
+        #region Variables
+        private DateTime breakDownDate;
+        private String note, advise, statusName;
+        private Int32 statusID;
+        #endregion
+
+        #region Properties
+        public DateTime BreakDownDate
+        {
+            get { return breakDownDate; }
+            set
+            {
+                if (breakDownDate != value)
+                {
+                    breakDownDate = value;
+                    RaisePropertyChanged("BreakDownDate");
+                }
+            }
+        }
+        public String Note
+        {
+            get { return note; }
+            set
+            {
+                if (note != value)
+                {
+                    note = value;
+                    RaisePropertyChanged("Note");
+                }
+            }
+        }
+        public Int32 StatusID
+        {
+            get { return statusID; }
+            set
+            {
+                if (statusID != value)
+                {
+                    statusID = value;
+                    RaisePropertyChanged("StatusID");
+                }
+            }
+        }
+        public String StatusName
+        {
+            get { return statusName; }
+            set
+            {
+                if (statusName != value)
+                {
+                    statusName = value;
+                    RaisePropertyChanged("StatusName");
+                }
+            }
+        }
+        #endregion
+
+        public EquipmentBreakDetailViewModel() : base()
+        {
+            BreakDownDate = DateTime.Now;
+        }
+    }
+
+    class EquipmentBreakViewModel : EquipmentViewModel
+    {
+        #region Variables
+        private Int32 companyID;
+        private String companyName;
+        private DateTime repairDate;
+        private String advise;
+        #endregion
+
+        #region Properties
+        public Int32 CompanyID
+        {
+            get { return companyID; }
+            set
+            {
+                if (companyID != value)
+                {
+                    companyID = value;
+                    RaisePropertyChanged("CompanyID");
+                }
+            }
+        }
+        public String CompanyName
+        {
+            get { return companyName; }
+            set
+            {
+                if (companyName != value)
+                {
+                    companyName = value;
+                    RaisePropertyChanged("CompanyName");
+                }
+            }
+        }
+        public DateTime RepairDate
+        {
+            get { return repairDate; }
+            set
+            {
+                if (repairDate != value)
+                {
+                    repairDate = value;
+                    RaisePropertyChanged("RepairDate");
+                }
+            }
+        }
+        public String Result { get; set; }
+        public String Advise
+        {
+            get { return advise; }
+            set
+            {
+                if (advise != value)
+                {
+                    advise = value;
+                    RaisePropertyChanged("Advise");
+                }
+            }
+        }
+        #endregion
+
+        public EquipmentBreakViewModel() : base()
+        {
+            RepairDate = DateTime.Now;
+        }
+    }
+}
---
+++ ERPManagement/ERPManagement/ViewModel/Equipment/EquipmentExportationViewModel.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using Telerik.Windows.Controls;
+
+namespace ERPManagement.ViewModel.Equipment
+{
+    class EquipmentExportationDetailViewModel : EquipmentDetailViewModel
+    {
+        #region Variables
+        private Int32 quantity, statusID;
+        private String statusName, note;
+        #endregion
+
+        #region Properties
+        public Int32 Quantity
+        {
+            get { return quantity; }
+            set
+            {
+                if (quantity != value)
+                {
+                    quantity = value;
+                    RaisePropertyChanged("Quantity");
+                }
+            }
+        }
+        public Int32 StatusID
+        {
+            get { return statusID; }
+            set
+            {
+                if (statusID != value)
+                {
+                    statusID = value;
+                    RaisePropertyChanged("StatusID");
+                }
+            }
+        }
+        public String StatusName
+        {
+            get { return statusName; }
+            set
+            {
+                if (statusName != value)
+                {
+                    statusName = value;
+                    RaisePropertyChanged("StatusName");
+                }
+            }
+        }
+        public String Note
+        {
+            get { return note; }
+            set
+            {
+                if (note != value)
+                {
+                    note = value;
+                    RaisePropertyChanged("Note");
+                }
+            }
+        }
+        #endregion
+
+        public EquipmentExportationDetailViewModel() : base()
+        {
+
+        }
+    }
+
+    class EquipmentExportationViewModel : EquipmentViewModel
+    {
+        #region Variables
+        private Int32 sender;
+        private String senderName;
+        private Int32 equipmentExportationID = 0;
+        #endregion
+
+        #region Properties
+        public Int32 EquipmentExportationID
+        {
+            get { return equipmentExportationID; }
+        }
+        public Int32 Sender
+        {
+            get { return sender; }
+            set
+            {
+                if (sender != value)
+                {
+                    sender = value;
+                    RaisePropertyChanged("Sender");
+                }
+            }
+        }
+        public String SenderName
+        {
+            get { return senderName; }
+            set
+            {
+                if (senderName != value)
+                {
+                    senderName = value;
+                    RaisePropertyChanged("SenderName");
+                }
+            }
+        }
+        public ObservableCollection<EquipmentExportationDetailViewModel> Details { get; set; }
+        #endregion
+
+        public EquipmentExportationViewModel()
+        {
+            Details = new ObservableCollection<EquipmentExportationDetailViewModel>();
+        }
+
+        protected override void Save(RadWindow window)
+        {
+        }
+
+        protected override bool Delete()
+        {
+            return base.Delete();
+        }
+
+        protected override void Edit()
+        {
+
+        }
+    }
+}
