5ce43ba486e52171dad1ac79d93540ea292ece5b
DESKTOP-HVT366J\VIETQUYTINA <vietquytina@gmail.com>
2017-02-20 22:55:21 +0700

    + add Authorize attribute. + add Employee, Department view model. + Delete Class, FundingSource, Grade, Subject.

---
+++ ERPManagement/ERPManagement/ViewModel/Authorize/AuthorizeAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ERPManagement.ViewModel.Authorize
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    class AuthorizeAttribute : Attribute
+    {
+        public String Method { get; set; }
+
+        public AuthorizeAttribute()
+        {
+
+        }
+    }
+}
---
+++ ERPManagement/ERPManagement/ViewModel/Employee/EmployeeViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ERPManagement.ViewModel.Employee
+{
+    public class PermissionViewModel
+    {
+        public String MethodName { get; set; }
+        public Boolean CanRead { get; set; }
+        public Boolean CanWrite { get; set; }
+        public Boolean CanDelete { get; set; }
+    }
+
+    [Authorize.Authorize(Method = "Employee")]
+    public class EmployeeViewModel : BaseViewModel
+    {
+        public Int32 UserID { get; set; }
+        public String Name { get; set; }
+        public List<PermissionViewModel> Permissions { get; set; }
+
+        public EmployeeViewModel()
+        {
+            Permissions = new List<PermissionViewModel>();
+        }
+    }
+}
