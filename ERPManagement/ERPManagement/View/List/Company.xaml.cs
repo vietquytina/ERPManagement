b683a54b0dbe177de3ad1605da25d69113b9dd7f
DESKTOP-HVT366J\VIETQUYTINA <vietquytina@gmail.com>
2017-02-23 07:32:26 +0700

    + add Company view.

---
+++ ERPManagement/ERPManagement/View/List/Company.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ERPManagement.View.List
+{
+    /// <summary>
+    /// Interaction logic for Company.xaml
+    /// </summary>
+    public partial class Company : Telerik.Windows.Controls.RadWindow
+    {
+        public Company()
+        {
+            InitializeComponent();
+        }
+    }
+}
